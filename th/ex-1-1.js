f8719f69a7777dc502192f00b6bc5d181318b579
CodePawsDev <isripen1991@outlook.com>
2024-10-16 17:29:55 +0700

    feat: change exercise 1.1and exercise 1.1 answer

---
+++ th/ex-1-1.js
@@ -0,0 +1,19 @@
+/* 
+- ให้เขียน Function ที่ชื่อว่า exponentialNumber ซึ่งมีคุณสมบัติต่อไปนี้
+    - รับ Parameter 1 ตัวคือ numbers
+        1. numbers เป็น Array ที่บรรจุตัวเลข
+    - Function นี้จะนำตัวเลขทุกตัวใน numbers มายกกำลังสอง แล้ว Return ค่าออกมา
+    - เมื่อ Algorithm นี้ทำงาน จะต้องเห็นผลลัพธ์ด้านล่าง
+*/
+
+// Start coding here
+
+function exponentialNumber(numbers) {
+  return numbers.map((number) => number ** 2);
+}
+
+const numbers1 = [3, 5, 10, 12];
+console.log(exponentialNumber(numbers1)); // [ 9, 25, 100, 144 ]
+
+const number2 = [0, -1, -4, -8];
+console.log(exponentialNumber(number2)); // [ 0, 1, 16, 64 ]
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,20 +1,71 @@
 /* 
-- ให้เขียน Function ที่ชื่อว่า sumNegativeNumbers ซึ่งมีคุณสมบัติต่อไปนี้
-    - รับ Parameter 1 ตัวคือ numbers
-        1. numbers เป็น Array ที่บรรจุตัวเลข
-    - Function นี้จะนำเฉพาะตัวเลขที่มีค่าน้อยกว่าศูนย์มารวมกัน แล้ว Return ค่าออกมา
+- ให้เขียน Function ที่ชื่อว่า addBonusForSelectedDepartment ซึ่งมีคุณสมบัติต่อไปนี้
+    - รับ Parameter 3 ตัวคือ employeeInfo, bonus และ department
+        1. employeeInfo เป็น Array ที่บรรจุข้อมูลพนักงาน
+        2. bonus เป็น Number ที่บรรจุจำนวนเงินโบนัส
+        3. department เป็น String ที่บรรจุชื่อแผนกที่จะเพิ่มโบนัสให้
+    - Function นี้จะนำ employeeInfo มาตรวจสอบแล้วเลือกเฉพาะคนที่อยู่ในแผนกที่จะเพิ่มโบนัสให้ เพื่อเพิ่มโบนัสตามจำนวนเงินที่ระบุไว้ แล้วนำมาคำนวณว่าเจ้าของบริษัทจะต้องใช้เงินทั้งหมดเท่าไร เพื่อจ่ายเงินเดือนเดือนนั้นให้พนักงานในแผนกที่กำหนด จากนั้นให้ Return ค่าออกมา
     - เมื่อ Algorithm นี้ทำงาน จะต้องเห็นผลลัพธ์ด้านล่าง
 */
 
 // Start coding here
-function sumNegativeNumbers(numbers) {
-  return numbers
-    .filter((number) => number < 0)
+function addBonusForSelectedDepartment(employeeInfo, bonus, department) {
+  return employeeInfo
+    .filter((employee) => employee.department === department)
+    .map((employee) => employee.salary + bonus)
     .reduce((acc, cur) => acc + cur, 0);
 }
 
-const numbers1 = [3, -5, 10, 12, -7, 0, 8, 2];
-console.log(sumNegativeNumbers(numbers1)); // -12
+const employeeInfo = [
+  {
+    firstname: "Alice",
+    lastName: "Redfield",
+    department: "IT",
+    salary: 25000,
+  },
+  {
+    firstname: "John",
+    lastName: "Doe",
+    department: "HR",
+    salary: 30000,
+  },
+  {
+    firstname: "Emily",
+    lastName: "Clark",
+    department: "Finance",
+    salary: 35000,
+  },
+  {
+    firstname: "Michael",
+    lastName: "Brown",
+    department: "IT",
+    salary: 27000,
+  },
+  {
+    firstname: "James",
+    lastName: "Taylor",
+    department: "HR",
+    salary: 29000,
+  },
+  {
+    firstname: "Jessica",
+    lastName: "Johnson",
+    department: "Finance",
+    salary: 33000,
+  },
+  {
+    firstname: "Robert",
+    lastName: "Miller",
+    department: "Operations",
+    salary: 31000,
+  },
+  {
+    firstname: "David",
+    lastName: "Wilson",
+    department: "Operations",
+    salary: 34000,
+  },
+];
 
-const number2 = [0, -4, -1, -5, 6, 5, 0];
-console.log(sumNegativeNumbers(number2)); // -10
+console.log(addBonusForSelectedDepartment(employeeInfo, 5000, "HR")); // 69000
+console.log(addBonusForSelectedDepartment(employeeInfo, 1000, "Operations")); // 67000

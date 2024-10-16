2d3ed965ae7390a968006168f749635d4b5811eb
CodePawsDev <isripen1991@outlook.com>
2024-10-16 17:26:39 +0700

    feat: add initial exercises in thai language

---
+++ th/ex-1-1.js
@@ -0,0 +1,15 @@
+/* 
+- ให้เขียน Function ที่ชื่อว่า exponentialNumber ซึ่งมีคุณสมบัติต่อไปนี้
+    - รับ Parameter 1 ตัวคือ numbers
+        1. numbers เป็น Array ที่บรรจุตัวเลข
+    - Function นี้จะนำตัวเลขทุกตัวใน numbers มายกกำลังสอง แล้ว Return ค่าออกมา
+    - เมื่อ Algorithm นี้ทำงาน จะต้องเห็นผลลัพธ์ด้านล่าง
+*/
+
+// Start coding here
+
+const numbers1 = [3, 5, 10, 12];
+console.log(exponentialNumber(numbers1)); // [ 9, 25, 100, 144 ]
+
+const number2 = [0, -1, -4, -8];
+console.log(exponentialNumber(number2)); // [ 0, 1, 16, 64 ]
---
+++ th/ex-1-2.js
@@ -0,0 +1,15 @@
+/* 
+- ให้เขียน Function ที่ชื่อว่า sumNegativeNumbers ซึ่งมีคุณสมบัติต่อไปนี้
+    - รับ Parameter 1 ตัวคือ numbers
+        1. numbers เป็น Array ที่บรรจุตัวเลข
+    - Function นี้จะนำเฉพาะตัวเลขที่มีค่าน้อยกว่าศูนย์มารวมกัน แล้ว Return ค่าออกมา
+    - เมื่อ Algorithm นี้ทำงาน จะต้องเห็นผลลัพธ์ด้านล่าง
+*/
+
+// Start coding here
+
+const numbers1 = [3, -5, 10, 12, -7, 0, 8, 2];
+console.log(sumNegativeNumbers(numbers1)); // -12
+
+const number2 = [0, -4, -1, -5, 6, 5, 0];
+console.log(sumNegativeNumbers(number2)); // -10
